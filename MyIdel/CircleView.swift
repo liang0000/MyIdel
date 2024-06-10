ece608dffbde62525877be3392c6837b50268195
Liang0 <saurabh.karajgikar@dhl.com>
2024-06-10 10:50:14 +0800

    qwe

---
+++ MyIdel/CircleView.swift
@@ -0,0 +1,14 @@
+//
+
+
+import SwiftUI
+
+struct CircleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CircleView()
+}
